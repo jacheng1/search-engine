c707110137dce2e6fe4a2b90cb339e32ea3c4e77
remosrulloda <remosrulloda@gmail.com>
2024-11-09 17:15:27 -0800

    Fixes

--- indexer.py
+++ indexer.py
@@ -2,34 +2,43 @@ import os
 import json
 from bs4 import BeautifulSoup
 from collections import defaultdict
-from tokenizers.remo import *
+from nltk.stem import PorterStemmer
+from tokenizers.remo.PartA import * # change to your desired tokenizer
 
 # Tokens: all alphanumeric sequences in the dataset.
 # Stop words: do not use stopping, i.e. use all words, even the frequently occurring ones.
 # Stemming: use stemming for better textual matches. Suggestion: Porter stemming.
 # Important words: Words in bold, in headings (h1, h2, h3), and in titles should be treated as more important than the other words.
 
-def indexer():
+ps = PorterStemmer()
+
+inverted_index = defaultdict(list)
+
+
+def build_index(html_dir):
+    doc_id = 0
+
+    
+
+
+def indexer(html_dir):
     try:
-        os.mkdir("directory.txt")
+        os.mkdirs("directory.txt", exists_ok=True)
+
     except FileExistsError as e:
         print(e)
         return
-    
-
-def stem_tokens(self, tokens):
-    return ""
-
-def add_document(self, doc_id, text):
-    return ""
 
 
-def process_html(self, file_path):
-    return ""
+def tokenize_text(text):
+    return tokenize(text)
 
 
-def build_index(self, html_dir):
-    return ""
+def stem_tokens(tokens):
+    return [ps.stem(token) for token in tokens]
+
+
+
 
 
 
