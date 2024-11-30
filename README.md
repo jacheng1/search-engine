e32053cc263d67f8c6f42883efc4624edd00cc21
Jacky Cheng <75710146+jacheng1@users.noreply.github.com>
2024-11-29 22:11:37 -0800

    Add basic web interface layout.

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
-# Search Engine
+# Zotsearch
 
 ## Overview
+
+A search engine, built from the ground up, that utilizes a corpus of over 55,000 web pages, achieving results in less than 300ms. There are two main steps to our search engine: indexing and searching. Indexing is run as a preprocessing step, which creates a lookup table that stores all of the unique tokens (words) found in the corpus, along with the page in which they were located and a weight associated with its relevancy to the page. The search component handles user-defined queries in the search bar, enabling the engine to scour the index of 55,000 pages to deliver all relevant results.
 
 
 
 ## Built With
+
+* [![Python][python.com]][Python-url]
+* [![JavaScript][javascript.com]][JavaScript-url]
+* [![HTML][html.com]][HTML-url]
+* [![CSS][css.com]][CSS-url]
 
 
 
@@ -18,3 +25,16 @@ python api.py
 cd frontend
 npm run dev
 ```
+
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[python.com]: https://img.shields.io/badge/logo-python-yellow?logo=python
+[Python-url]: https://www.python.org/
+[javascript.com]: https://img.shields.io/badge/logo-javascript-blue?logo=javascript
+[JavaScript-url]: https://www.javascript.com/
+[html.com]: https://img.shields.io/badge/logo-html-blue?logo=html
+[HTML-url]: https://www.w3schools.com/html/
+[css.com]: https://img.shields.io/badge/logo-css-blue?logo=css
+[CSS-url]: https://www.w3.org/Style/CSS/Overview.en.html
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,5 +1,7 @@
 'use client'
 
+import SearchIcon from "@mui/icons-material/Search";
+import { Box, TextField, Typography, InputAdornment, IconButton } from "@mui/material";
 import { useState } from "react";
 
 export default function SearchPage() {
@@ -24,41 +26,82 @@ export default function SearchPage() {
       }
     } catch (error) {
       console.error("Error fetching results:", error);
-
     } finally {
       setLoading(false);
-
     }
   };
 
+  const colors = ["#4285F4", "#EA4335", "#FBBC05", "#34A853"];
+  const text = "Zotsearch";
+
   return (
-    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
-      <h1>Search Engine</h1>
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Enter your query"
+    <>
+      <Box style={{ padding: "25px", fontFamily: "Arial, sans-serif" }}>
+        <Box
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "40px",
+            marginBottom: "20px"
+          }}
+        >
+          <Typography variant="h4" component="div" style={{ display: "flex", alignItems: "center", margin: 0 }}>
+            {text.split("").map((char, index) => (
+              <span
+                key={index}
+                style={{
+                  color: colors[index % colors.length]
+                }}
+              >
+                {char}
+              </span>
+            ))}
+          </Typography>
+          <Box style={{ display: "flex", alignItems: "center" }}>
+            <TextField
+              variant="outlined"
+              placeholder="Search Zotsearch"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyDown={(e) => {
+                if ((e.key === "Enter") && (e !== "")) {
+                  handleSearch();
+                }
+              }}
+              InputProps={{
+                style: { borderRadius: "25px" },
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleSearch}>
+                      <SearchIcon sx={{ color: "#4285F4" }}/>
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+              style={{ width: "750px" }}
+            />
+          </Box>
+        </Box>
+      </Box>
+      <div
         style={{
-          padding: "10px",
-          width: "300px",
-          marginRight: "10px",
-          fontSize: "16px",
+          borderTop: "1px solid lightgray",
+          width: "100%",
+          marginLeft: 0,
+          marginRight: 0,
+          marginBottom: "20px"
         }}
-      />
-      <button onClick={handleSearch} style={{ padding: "10px 20px", fontSize: "16px" }}>
-        Search
-      </button>
-      {loading && <p>Loading...</p>}
-      <ul>
+      ></div>
+      <Box style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+        {loading && <Typography variant="h6">Loading...</Typography>}
         {results.map((result, index) => (
-          <li key={index}>
+          <Typography key={index}>
             <a href={result[0]} target="_blank" rel="noopener noreferrer">
               {result[0]} (Score: {result[1].toFixed(2)})
             </a>
-          </li>
+          </Typography>
         ))}
-      </ul>
-    </div>
+      </Box>
+    </>
   );
 }
